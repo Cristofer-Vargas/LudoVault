fc316b5e0121df0ec11ee73d4779e2abd2720307
Cristofer-Vargas <vargascristofer97@gmail.com>
2026-09-26 15:44:51 -0300

    fix(Response): Removendo lista de games da entidade
    - Removendo "public List<GameResponse> Games { get; set; } = [];" para
      seguir padrão de publisher

--- DTO/Responses/DeveloperResponse.cs
+++ DTO/Responses/DeveloperResponse.cs
@@ -4,6 +4,5 @@ namespace LudoVault.DTO.Responses
   {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public List<GameResponse> Games { get; set; } = [];
   }
 }
